26d5b6d29bd0ff63f1186ed011f4e07c4ed81b74
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-30 18:00:52 +0200

    Update CatalogBrand

--- Catalog/Catalog.Host/Controllers/CatalogBrandController.cs
+++ Catalog/Catalog.Host/Controllers/CatalogBrandController.cs
@@ -1,3 +1,8 @@
+using System.Net;
+using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Requests;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Services.Interfaces;
 using Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +13,32 @@ namespace Catalog.Host.Controllers;
 public class CatalogBrandController : ControllerBase
 {
     private readonly ILogger<CatalogBrandController> _logger;
+    private readonly ICatalogBrandService _brandService;
 
-    public CatalogBrandController(ILogger<CatalogBrandController> logger)
+    public CatalogBrandController(ILogger<CatalogBrandController> logger, ICatalogBrandService brandService)
     {
         _logger = logger;
+        _brandService = brandService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(string brand)
+    {
+        var result = await _brandService.Add(brand);
+        return Ok(new AddItemResponse<int?>() { Id = result });
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int itemId)
+    {
+        var result = _brandService.Delete(itemId);
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put(CatalogBrandDto item, int itemToUpdate)
+    {
+        var result = await _brandService.Put(item, itemToUpdate);
+        return Ok();
     }
 }
