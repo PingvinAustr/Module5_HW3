a68b8f52d7c72d56889241f997ff795c8eeaa97c
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2023-01-30 18:00:31 +0200

    Update CatalogBffController.cs

--- Catalog/Catalog.Host/Controllers/CatalogBffController.cs
+++ Catalog/Catalog.Host/Controllers/CatalogBffController.cs
@@ -31,4 +31,69 @@ public class CatalogBffController : ControllerBase
         var result = await _catalogService.GetCatalogItemsAsync(request.PageSize, request.PageIndex);
         return Ok(result);
     }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(CatalogItemDto), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var item = await _catalogService.GetById(id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(item);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetByBrand(int brandId)
+    {
+        var items = await _catalogService.GetByBrand(brandId);
+        if (items == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(items);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetByType(int typeId)
+    {
+        var items = await _catalogService.GetByType(typeId);
+        if (items == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(items);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogBrandDto>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetBrands()
+    {
+        var items = await _catalogService.GetBrands();
+        if (items == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(items);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(PaginatedItemsResponse<CatalogTypeDto>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetTypes()
+    {
+        var items = await _catalogService.GetTypes();
+        if (items == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(items);
+    }
 }
